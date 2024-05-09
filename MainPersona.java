e1ef55be1ac16672ed712d4013a599a865a2663a
unknown <rantonioivann@gmail.com>
2024-05-08 21:05:03 -0600

    v2

--- MainPersona.java
+++ MainPersona.java
@@ -1,23 +1,34 @@
-import java.util.Scanner;
-public class MainPersona{
-    public static void main(String[] args) {
-        Scanner p1 = new Scanner(System.in);
-        System.out.println("Ingresa tu nombre: ");
-        String nombre = p1.next();
-        System.out.println("Ingresa tu apellido: ");
-        String apellido = p1.next();
-        System.out.println("Cual es tu edad: ");
-        int edad = p1.nextInt();
-        System.out.println("Cuanto mides: ");
-        double altura = p1.nextDouble();
-        Persona p2 = new Persona();
-        p2.nombre=nombre;
-        p2.apellido=apellido;
-        p2.edad=edad;
-        p2.altura=altura;
-        nombre=p2.mostrarNombre();
-        apellido=p2.mostrarApellido();
-        edad=p2.mostrarEdad();
-        altura=p2.mostrarAltura();
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
+
+public class MainPersona {
+    public static void main(String[] args) { // Método principal, punto de entrada del programa
+        Scanner p1 = new Scanner(System.in); // Crea un objeto Scanner llamado p1 para leer la entrada del usuario
+
+        System.out.println("Ingresa tu nombre: "); // Solicita al usuario que ingrese su nombre
+        String nombre = p1.next(); // Lee el nombre proporcionado por el usuario
+
+        System.out.println("Ingresa tu apellido: "); // Solicita al usuario que ingrese su apellido
+        String apellido = p1.next(); // Lee el apellido proporcionado por el usuario
+
+        System.out.println("Cual es tu edad: "); // Solicita al usuario que ingrese su edad
+        int edad = p1.nextInt(); // Lee la edad proporcionada por el usuario
+
+        System.out.println("Cuanto mides: "); // Solicita al usuario que ingrese su altura
+        double altura = p1.nextDouble(); // Lee la altura proporcionada por el usuario
+
+        Persona p2 = new Persona(); // Crea un objeto de la clase Persona llamado p2
+
+        // Asigna los valores proporcionados por el usuario al objeto p2
+        p2.nombre = nombre;
+        p2.apellido = apellido;
+        p2.edad = edad;
+        p2.altura = altura;
+
+        // Llama a los métodos mostrarNombre(), mostrarApellido(), mostrarEdad() y mostrarAltura()
+        // de p2 para obtener los valores actualizados y asignarlos a las variables correspondientes
+        nombre = p2.mostrarNombre();
+        apellido = p2.mostrarApellido();
+        edad = p2.mostrarEdad();
+        altura = p2.mostrarAltura();
     }
 }
--- Persona.java
+++ Persona.java
@@ -1,25 +1,75 @@
 public class Persona {
 
-    String nombre;
-    String apellido;
-    int edad;
-    double altura;
+    // Declaramos variables para almacenar información personal
+    String nombre; // Nombre de la persona
+    String apellido; // Apellido de la persona
+    int edad; // Edad de la persona
+    double altura; // Altura de la persona
     
+    // Método para mostrar el nombre de la persona
     public String mostrarNombre(){
-        System.out.println("Hola me llamo: "+this.nombre);
+        System.out.println("Hola me llamo: "+this.nombre); // Imprime el nombre de la persona
+        return nombre; // Devuelve el nombre
+    }
+    
+    // Método para mostrar el apellido de la persona
+    public String mostrarApellido(){
+        System.out.println("Y mi apellido es: "+this.apellido); // Imprime el apellido de la persona
+        return apellido; // Devuelve el apellido
+    }         
+    
+    // Método para mostrar la edad de la persona
+    public int mostrarEdad(){
+        System.out.println("Mi edad es: "+this.edad); // Imprime la edad de la persona
+        return edad; // Devuelve la edad
+    }     
+    
+    // Método para mostrar la altura de la persona
+    public double mostrarAltura(){
+        System.out.println("Y mi altura es : "+this.altura); // Imprime la altura de la persona
+        return altura; // Devuelve la altura
+    }
+
+    // Métodos getter y setter para acceder y modificar el nombre
+    public String getNombre() {
         return nombre;
     }
-    public String mostrarApellido(){
-        System.out.println("Y mi apellido es: "+this.apellido);
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Métodos getter y setter para acceder y modificar el apellido
+    public String getApellido() {
         return apellido;
-    }         
-    public int mostrarEdad(){
-        System.out.println("Mi edad es: "+this.edad);
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    // Métodos getter y setter para acceder y modificar la edad
+    public int getEdad() {
         return edad;
-    }     
-    public double mostrarAltura(){
-        System.out.println("Y mi altura es : "+this.altura);
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Métodos getter y setter para acceder y modificar la altura
+    public double getAltura() {
         return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    // Método toString que devuelve una representación de cadena de la información de la persona
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
     }
 
 }
